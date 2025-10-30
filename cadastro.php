c8181c05143b5284e830e17deac005583fc183fe
Jailson Torquato <36706453+JailsonTorquato@users.noreply.github.com>
2025-10-30 19:32:12 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,1 +1,23 @@
+<?php
+    include('conexao.php');
+   if (!isset($_SESSION)){
+        session_start();
+    }
+    if (!isset($_SESSION['usuario'])){
+        header('Location: login.php');
+        die();
+    }
 
+    function limpar_texto($str){
+        return preg_replace("/[^0-9]/","",$str);
+    }
+
+    if (count($_POST)>0){
+        include('conexao.php');
+        $erro = false;
+        $nome  = $_POST['nome'];
+        $email = $_POST['email'];
+        
+    }
+
+?>
